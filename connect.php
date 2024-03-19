3244649a320e72be204d368fe200147e551714ab
Lê Trương Nhật Nam <130050184+namdicodedao@users.noreply.github.com>
2024-03-19 20:36:01 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,11 @@
 <?php
-$server = 'localhost';
-$user = 'root';
-$pass = '';
-$database = 'webtintuc';
+$server = 'lmag6s0zwmcswp5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$user = 'cpwl2jxjzqbpy7h9';
+$pass = 'wud9bjhkyjvi0kpk ';
+$database = 'k32zmwr9mjdnbp3k';
+$port = '3306';
 
-$conn = mysqli_connect($server, $user, $pass, $database);
+$conn = mysqli_connect($server, $user, $pass, $database,$port);
 if ($conn) {
     mysqLi_query($conn, "SET NAMES 'utf8' ");
 } else {
