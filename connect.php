69bb906ab5cac2280b68a31a7458bc0b3b1b935a
Lê Trương Nhật Nam <130050184+namdicodedao@users.noreply.github.com>
2024-04-06 00:29:05 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$server = 'host.docker.internal';
-$user = 'root';
-$pass = '123456';
-$database = 'webtintuc';
+$server = 'm7wltxurw8d2n21q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$user = 'd78co66csqefqxnq';
+$pass = 'mj350gtj90n70zmb';
+$database = 'rhpmjug5tcbd919';
 
 $conn = mysqli_connect($server, $user, $pass, $database);
 if ($conn) {
